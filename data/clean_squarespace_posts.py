59ed20e26b5e4e8d11cefda6185a9edc84b67d85
david cochran <about.trout@gmail.com>
2020-04-24 19:03:14 -0700

    data cleaning improvements

--- data/clean_squarespace_posts.py
+++ data/clean_squarespace_posts.py
@@ -2,45 +2,46 @@
 
 import os
 import re
+import sys
 
 from bs4 import BeautifulSoup
 from unicodedata import normalize
 
 
-def sanitize_string(body):
-    x = body.strip()
-    x = re.sub("\n", " ", x)
-    x = re.sub("\s+", " ", x)
-    x = re.sub(" , ", ", ", x)
-    return x
-
-
-def parse_posts_from_page(page_file, out_file):
+def parse_posts_from_page(page_file, out_file, known_filler):
     soup = BeautifulSoup(open(page_file), "html.parser")
+    num_skipped = 0
     for e in soup.find_all("article", {"class": "entry"}):
         # Grab text contents from .entry-content <div>s.
-        content = e.find("div", {"class": "entry-content"}).get_text(" ")
+        content = e.find("div", {"class": "entry-content"}).get_text("")
+        # Sanitize and fix up CHANNEL/PATCH headers.
         content = sanitize_string(content)
-        # Fix up CHANNEL/PATCH headers.
         content = content.replace("CHANNEL :", "CHANNEL:")
         content = content.replace("PATCH :", "PATCH:")
         # Trim beginning/end of post, hopefully only getting the report.
         start_offset = min(
                 max(0, content.find("PATCH:")),
                 max(0, content.find("CHANNEL:")))
-        end_offset = check_for_end_filler_text(content)
-        # Bon voyage, content.
-        yield content[start_offset:end_offset]
+        content = content[start_offset:]
+        end_offset = find_first_end_offset(content, known_filler)
+        content = content[:end_offset].strip()
+        # FIXMEUP.
+        if content.startswith("CHANNEL:"):
+            offset = content.find("PATCH:")
+            channel = content[:offset]
+            patch = content[offset:]
+            yield channel, patch
+        elif content.startswith("PATCH:"):
+            offset = content.find("CHANNEL:")
+            channel = content[:offset]
+            patch = content[offset:]
+            yield channel, patch
+        else:
+            num_skipped += 1
+    print(f"Skipped posts from page: page={page_file}, num_skipped={num_skipped}")
 
 
-def check_for_end_filler_text(content):
-    known_filler = [
-        "PACK IT IN! PACK IT OUT!",
-        "RENTAL INVENTORY SALE!",
-        "PACK IT IN. PACK IT OUT.",
-        "Join us Thanksgiving weekend for",
-        "Don't forget - our Storewide",
-    ]
+def find_first_end_offset(content, known_filler):
     return min([find_end_offset(content, kf) for kf in known_filler])
 
 
@@ -51,10 +52,29 @@ def find_end_offset(content, kf):
     return offset
 
 
+def sanitize_string(body):
+    x = body.strip()
+    x = re.sub("\n", " ", x)
+    x = re.sub("\s+", " ", x)
+    x = re.sub(" , ", ", ", x)
+    return x
+
+
 if __name__ == "__main__":
+    # Fixed set of common surf report endings.
+    known_filler = [
+        "PACK IT IN",
+        "RENTAL INVENTORY",
+        "2 Mile Surf Shop Rental Gear",
+        "NOTE:",
+        "LIMITED SEATING",
+        "The second annual Mill Valley Surf Film",
+    ]
+    # Parse each HTML post separately and save text to another file.
     raw_dir = "squarespace_raw"
-    with os.scandir(raw_dir) as it, open("squarespace_posts.txt", "w") as out_file:
-        for entry in it:
+    with open("squarespace_posts.txt", "w") as out_file:
+        for entry in os.scandir(raw_dir):
             if entry.name.endswith(".html") and entry.is_file():
-                for post in parse_posts_from_page(entry.path, out_file):
-                    out_file.write(post + "\n")
+                for channel, patch in parse_posts_from_page(entry.path, out_file, known_filler):
+                    out_file.write(channel + "\n")
+                    out_file.write(patch + "\n")
